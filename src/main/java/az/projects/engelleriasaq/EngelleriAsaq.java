efdfa8650ffe9d168b6b52435f0afbdb6e5685a8
Sakit Atakişiyev <sakit.atakishiyev@gmail.com>
2019-06-04 05:38:40 +0400

    Yeni classın yaradılması və main metodun əlavə olunması

---
+++ src/main/java/az/projects/engelleriasaq/EngelleriAsaq.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package az.projects.engelleriasaq;
+
+/**
+ *
+ * @author sanco
+ */
+public class EngelleriAsaq {
+    public static void main(String[] args){
+        System.out.println("Əngəlləri Birlikdə Aşaq!");
+    }
+}
